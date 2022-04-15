9cb2263bd38b784a5b5447a9c5d8eed2c5ee8ecf
Min Young Choe <97721636+MinYoungChoe@users.noreply.github.com>
2022-04-15 10:47:08 -0400

    Add files via upload

---
+++ w1p1.c
@@ -0,0 +1,16 @@
+/*
+ Name : Min Young Choe
+ Id: 133607218
+ Email : mcho@senecacollege.ca
+ Section: ZBB
+ */
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("*** Welcome to Seneca's C Programming Course ***\n");
+    printf("\n");
+    
+    return 0; //This means the program ran successfully
+}
---
+++ w1p2.c
@@ -0,0 +1,19 @@
+/*
+ Name : Min Young Choe
+ Id: 133607218
+ Email : mcho@senecacollege.ca
+ Section: ZBB
+ */
+
+#include <stdio.h>
+
+int main() {
+    
+    printf("+-------------------------------+\n");
+    printf("| Introduction to C Programming |\n");
+    printf("+-------------------------------+\n");
+    printf("Program written by:\n");
+    printf("Min Young Choe\n");
+    
+    return 0;
+}
